2953f1860b1c4bd566df3b947b2e1240b12ee0f7
Nay Htet Aung <nayhtetaung201733@gmail.com>
2021-04-15 21:39:53 +0630

    Project 1 Deployed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   purge: [
     './src/**/*.html',
-    './src/**/*.js',
+    './src/**/*.vue',
+    './src/**/*.jsx',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
